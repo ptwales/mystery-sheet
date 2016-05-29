7556667214eb261b04fac5d0027a7c8dcc1da49d
Philip Wales <ptwales@openmailbox.org>
2016-05-29 16:37:57 -0500

    checks

--- src/test/scala/RandomCSV.scala
+++ src/test/scala/RandomCSV.scala
@@ -3,6 +3,8 @@ package com.ptwales.sheets
 import java.nio.file.Path
 import java.nio.charset.Charset
 
+import scala.collection.JavaConverters._
+
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -18,6 +20,14 @@ class RandomCSVGenerator extends FunSuite {
   def randomCharSet(): Charset = ???
   def makeFileText(data: Data, c: Char, r: Char): String = ???
   def writeToFile(text: String): Path = ???
-  def readFromFile(path: Path): CSVSheet = ???
+  def readFromFile(path: Path): DataSheet = ???
 
+  // we will test ALL charsets allowed
+  val charsetsToTest = Charset.availableCharsets.asScala
+  val testsPerCharset = 5
+  for ((k, v) <- charsetsToTest) {
+    test(s"Running $testsPerCharset tests for $k") {
+    }
+  }
 }
+
