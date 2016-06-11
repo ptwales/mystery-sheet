232751c37122e1007be697dd7d95877641c8d69c
Philip Wales <ptwales@openmailbox.org>
2016-06-11 13:03:01 -0500

    Cleaned up helper classes

--- src/test/scala/RandomCSV.scala
+++ src/test/scala/RandomCSV.scala
@@ -10,11 +10,8 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-
 @RunWith(classOf[JUnitRunner])
-class RandomCSVGenerator extends FunSuite {
-
-  type Data = Vector[Vector[String]]
+class RandomCSV extends FunSuite {
 
   val testsPerCharset = 10
   val maxColCount = 10
@@ -23,207 +20,204 @@ class RandomCSVGenerator extends FunSuite {
 
   val charsetsToTest = Seq("UTF-8").map(Charset.forName(_))
 
+
   val tests = for { 
     charset <- charsetsToTest.toStream
     i <- (1 to testsPerCharset)
-  } yield  CSVTestData(charset, maxColCount, maxRowCount, maxCellSize)
+  } yield CSVTestCase(charset, maxColCount, maxRowCount, maxCellSize)
+    
+ 
 
-  tests foreach { t =>
+  tests foreach { testCase =>
 
-    val testCase = t.testCase
     val charset = testCase.charset
 
     test("Raw Test: " + testCase.name) {
-      val sheet = CSVSheet.fromText(t.text, testCase.colSep)
-      t.check(sheet)
+      val sheet = CSVSheet.fromText(testCase.text, testCase.colSep)
+      testCase.check(sheet)
     }
 
     test("File Test: " + testCase.name) {
       val safeName = testCase.name.replace("/", "SLASH")
       val dest = Paths.get(safeName + ".csv")
       try {
-        val written = Files.write(dest, t.lines.asJava, charset)
+        val written = Files.write(dest, testCase.lines.asJava, charset)
         val src = io.Source.fromURL(written.toUri.toURL)
         val sheet = CSVSheet.fromSource(src, testCase.colSep)
-        t.check(sheet)
+        testCase.check(sheet)
       } finally {
         Files.delete(dest)
       }
     }
   }
+}
 
-  def randomSize(max: Int): Int = {
-    Random.nextInt(max - 1) + 1
+/** Contains test conditons for a random CSV test.
+  */
+case class CSVTestCase(
+  charset: Charset, 
+  colCount: Int,
+  rowCount: Int, 
+  colSep: Char,
+  quote: Char,
+  maxCellSize: Int)
+  extends FunSuite { //probably a better class to extend
+
+
+  /** Formatted string showing test conditions */
+  lazy val name: String = {
+    s"charset:${charset.displayName}_" +
+    s"rows:${rowCount}_cols:${colCount}_" +
+    s"c:`${colSep}`_q:`${quote}`"
   }
 
-  /** Contains test conditons for a random CSV test. */
-  case class CSVTestCase(charset: Charset, 
-                         colCount: Int, rowCount: Int, 
-                         colSep: Char, quote: Char) {
+  /***************\
+   Generating Data
+  \***************/
 
-    /** Returns a [[CSVTestData]] object. */
-    def createData(maxCellSize: Int): CSVTestData = {
-      val data = Vector.fill(rowCount, colCount)(generateRandomCell(0.25))
-      CSVTestData(this, data)
-    }
+  lazy val data = Vector.fill(rowCount, colCount)(
+    generateRandomCell(0.25)
+  )
 
-    /** Formatted string showing test conditions */
-    lazy val name: String = {
-      s"charset:${charset.displayName}_" +
-      s"rows:${rowCount}_cols:${colCount}_" +
-      s"c:`${colSep}`_q:`${quote}`"
-    }
-
-    /** Generates random text without column separator or quote characters. */
-    private val textGen: TextGenerator = {
-      val tg = TextGenerator(charset)
-      tg.forbidden += colSep
-      tg.forbidden += quote
-      tg
-    }
-
-    /** Generates a random cell value which might be quoted. */
-    private def generateRandomCell(qProb: Double): String = {
-      val s = textGen.randomString(maxCellSize)
-      if (math.random < qProb) quote + s + quote
-      else s
-    }
-
+  /** Generates random text without column separator or quote characters. */
+  lazy val textGen: TextGenerator = {
+    val tg = TextGenerator(charset)
+    tg.forbidden += colSep
+    tg.forbidden += quote
+    tg
   }
 
-  /** Companion object for [[CSVTestCase]]. */
-  object CSVTestCase {
-
-    /** Generate a random [[CSVTestCase]] with size limitations. */
-    def apply(charset: Charset, maxCol: Int, maxRow: Int): CSVTestCase = {
-      val colCount = randomSize(maxCol)
-      val rowCount = randomSize(maxRow)
-      val tg = TextGenerator(charset)
-      val colSep = tg.randomChar
-      tg.forbidden += colSep
-      val quote = tg.randomChar
-      new CSVTestCase(charset, colCount, rowCount, colSep, quote)
-    }
+  /** Generates a random cell value which might be quoted. */
+  private def generateRandomCell(qProb: Double): String = {
+    val s = textGen.randomString(maxCellSize)
+    if (math.random < qProb) quote + s + quote
+    else s
   }
-  
-  /** Contains a [[CSVTestCase]] and random data */
-  case class CSVTestData(testCase: CSVTestCase, data: Data) {
 
-    /** Lines of the random data */
-    lazy val lines: Vector[String] = {
-      data.map(_.mkString(testCase.colSep.toString))
-    }
+  /**************\
+   Printing Data
+  \**************/
 
-    /** The random data as a string */
-    lazy val text: String = {
-      lines.mkString(System.lineSeparator)
-    }
+  /** Lines of the random data */
+  lazy val lines: Seq[String] = {
+    data.map(_.mkString(colSep.toString))
+  }
 
-    def check(sheet: DataSheet): Unit = {
+  /** The random data as a string */
+  lazy val text: String = {
+    lines.mkString(System.lineSeparator)
+  }
 
-      assert(sheet.rows.size == data.size,
-        s"First rows, sheet=${sheet.rows.head} data=${data.head}")
+  /****************\
+   Validating data
+  \****************/
 
-      for (r <- (0 until data.size)) {
+  def check(sheet: DataSheet): Unit = {
 
-        val drow = data(r)
-        val srow = sheet.rowAt(r)
+    assert(sheet.rows.size == data.size,
+      s"First rows, sheet=${sheet.rows.head} data=${data.head}")
 
-        assert(srow.size == drow.size, s"Row $r of data=$drow")
+    for (r <- (0 until data.size)) {
 
-        for (c <- (0 until srow.size)) {
-          checkCell(srow(c), drow(c))
-        }
-      }
-    }
+      val drow = data(r)
+      val srow = sheet.rowAt(r)
 
-    private def checkCell(c: Option[Any], d: String): Unit = {
-      c match {
-        case Some(x) => checkValue(x, d)
-        case None => assertEmpty(d)
-      }
-    }
+      assert(srow.size == drow.size, s"Row $r of data=$drow")
 
-    private def assertEmpty(d: String): Unit = {
-      checkValue("", d)
-    }
-
-    private def checkValue(c: Any, d: String): Unit = {
-      c match {
-        case s: String => assert(s == d)
-        case i: Int => assert(i == d.toInt)
-        case n: Double => assert(n == d.toDouble)
-        case _ => fail("Unsupported data type")
+      for (c <- (0 until srow.size)) {
+        checkCell(srow(c), drow(c))
       }
     }
   }
 
-  /** Companion object for [[CSVTestData]] */
-  object CSVTestData {
-    
-    /** Create a random [[CSVTestData]] with limitations. */
-    def apply(charset: Charset, maxCol: Int, maxRow: Int, 
-              maxCell: Int): CSVTestData = {
-      CSVTestCase(charset, maxCol, maxRow).createData(maxCell)
+  private def checkCell(c: Option[Any], d: String): Unit = {
+    c match {
+      case Some(x) => checkValue(x, d)
+      case None => assertEmpty(d)
     }
   }
 
-  /** Generates random characters that are in a given Charset.
-    *
-    * Can randomly and efficiently non-control latin characters that are
-    * encodeable by a charset.
-    *
-    * It also has a mutable set [[forbidden]] who's characters will not be 
-    * generated.
+  private def assertEmpty(d: String): Unit = {
+    checkValue("", d)
+  }
+
+  private def checkValue(c: Any, d: String): Unit = {
+    c match {
+      case s: String => assert(s == d)
+      case i: Int => assert(i == d.toInt)
+      case n: Double => assert(n == d.toDouble)
+      case _ => fail("Unsupported data type")
+    }
+  }
+}
+
+/** Companion object for [[CSVTestCase]].
+  */
+object CSVTestCase {
+
+  /** Generate a random [[CSVTestCase]] with size limitations. */
+  def apply(charset: Charset, colCount: Int, rowCount: Int, maxCell: Int): CSVTestCase = {
+    val tg = TextGenerator(charset)
+    val colSep = tg.randomChar
+    tg.forbidden += colSep
+    val quote = tg.randomChar
+    new CSVTestCase(charset, colCount, rowCount, colSep, quote, maxCell)
+  }
+}
+
+/** Generates random characters that are in a given Charset.
+  *
+  * Can randomly and efficiently non-control latin characters that are
+  * encodeable by a charset.
+  *
+  * It also has a mutable set [[forbidden]] who's characters will not be 
+  * generated.
+  */
+class TextGenerator(cs: Charset) {
+
+  /** The [[java.nio.charset.Charset]] characters are generated from. */
+  val charset: Charset = cs
+
+  /** Characters in this set will never be generated. */
+  var forbidden: Set[Char] = Set[Char]()
+
+  /** All chars this can generate, not omitting [[forbidden]]. 
+    * 
+    * TODO: replace with a Stream[Char] for efficiency.
     */
-  class TextGenerator(cs: Charset) {
-
-    /** The [[java.nio.charset.Charset]] characters are generated from. */
-    val charset: Charset = cs
-
-    /** Characters in this set will never be generated. */
-    var forbidden: Set[Char] = Set[Char]()
-
-    /** All chars this can generate, not omitting [[forbidden]]. 
-      * 
-      * TODO: replace with a Stream[Char] for efficiency.
-      */
-    lazy val allChars: String = {
-      val encoder = charset.newEncoder
-      val charRange = (Char.MinValue.toChar to Char.MaxValue.toChar).mkString
-      val encodeable: String = charRange.filter(encoder.canEncode(_))
-      val noControl = encodeable.replaceAll("[\\p{C}]", "")
-      val noExtension = noControl.replaceAll("[^\\p{InBasicLatin}]", "")
-      noExtension
-    }
-
-    /** Generate a random char from [[allChars]] that isn't in [[forbidden]].*/
-    def randomChar(): Char = {
-      var result: Char = '.'
-      do {
-        result = allChars(Random.nextInt(allChars.size))
-      } while (forbidden(result))
-      result
-    }
-
-    /** Generate a string of random characters between 1 to maxSize characters 
-      * long.
-      *
-      * Characters are drawn from [[randomChar]] and repeats are allowed.
-      *
-      * @param  maxSize Maximum size the random string may be.
-      * @returns  A random string no longer than maxSize
-      */
-    def randomString(maxSize: Int): String = {
-      val size = Random.nextInt(maxSize - 1) + 1
-      Seq.fill(size)(randomChar()).mkString
-    }
-
+  lazy val allChars: String = {
+    val encoder = charset.newEncoder
+    val charRange = (Char.MinValue.toChar to Char.MaxValue.toChar).mkString
+    val encodeable: String = charRange.filter(encoder.canEncode(_))
+    val noControl = encodeable.replaceAll("[\\p{C}]", "")
+    val noExtension = noControl.replaceAll("[^\\p{InBasicLatin}]", "")
+    noExtension
   }
 
-  /** Companion object for [[TextGenerator]]. */
-  object TextGenerator {
-    def apply(cs: Charset): TextGenerator = new TextGenerator(cs)
+  /** Generate a random char from [[allChars]] that isn't in [[forbidden]].*/
+  def randomChar(): Char = {
+    var result: Char = '.'
+    do {
+      result = allChars(Random.nextInt(allChars.size))
+    } while (forbidden(result))
+    result
   }
 
+  /** Generate a string of random characters between 1 to maxSize characters 
+    * long.
+    *
+    * Characters are drawn from [[randomChar]] and repeats are allowed.
+    *
+    * @param  maxSize Maximum size the random string may be.
+    * @returns  A random string no longer than maxSize
+    */
+  def randomString(maxSize: Int): String = {
+    val size = Random.nextInt(maxSize)
+    Seq.fill(size)(randomChar()).mkString
+  }
+}
+
+/** Companion object for [[TextGenerator]]. */
+object TextGenerator {
+  def apply(cs: Charset): TextGenerator = new TextGenerator(cs)
 }
